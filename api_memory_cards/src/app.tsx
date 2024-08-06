57fc3a35b7e0d038ec179bed545717e70931a7d8
DJ de Vlamingh <Embryonic.dev@gmail.com>
2024-08-06 10:56:45 +0200

    Add score, scoreMultiplier, highScore

--- api_memory_cards/src/app.tsx
+++ api_memory_cards/src/app.tsx
@@ -10,12 +10,13 @@ export function App() {
   const [gameWon, setGameWon] = useState(false);
   const [gameOver, setGameOver] = useState(false)
   const [loserCard, setLoserCard] = useState<Card[]>([])
+  const [highScore, setHighScore] = useState(localStorage.getItem('highScore') || '0');
+  const [scoreMultiplier, setScoreMultiplier] = useState(5)
+  const [score, setScore] = useState(0)
 
-  const selectedCards = visibleCards.filter(card => card.selected)
+  console.log(`Score: ${score}`)
+  console.log(`High Score: ${highScore}`)
 
-  console.log('selected cards:', selectedCards)
-  console.log('Cards:', allCards)
-  console.log('Visible cards:', visibleCards)
 
   // Fetch all Cards
   useEffect(() => {
@@ -29,36 +30,53 @@ export function App() {
     })();
   }, []);
 
-  // Set visible cards
+  useEffect(() => {
+    localStorage.setItem('highScore', highScore)
+  }, [highScore])
+
+  // Set visible cards && remove them from allCards
   useEffect(() => {
     if (allCards.length > 0 && !visibleCards.length) {
-      setVisibleCards(allCards.slice(0, 6));
-      setAllCards(allCards.slice(6))
+      setVisibleCards(allCards.slice(0, 2));
+      setAllCards(allCards.slice(2));
     } else if (visibleCards.length > 0 && visibleCards.every(card => card.selected)) {
-      console.log('all cards selected!')
-      setVisibleCards(prevVisible  => {
-        const newVisible = [...prevVisible, ...allCards.slice(0, 6)];
-        setAllCards(allCards.slice(6))
-        return shuffleArray(newVisible)
-      })
+      if (visibleCards.length === 12) {
+        setScore(prevState => prevState + scoreMultiplier)
+        getHighScore()
+        setGameWon(true)
+        // Don't do anything else, game is over
+      } else {
+        setVisibleCards(prevVisible => {
+          const newVisible = [...prevVisible, ...allCards.slice(0, 2)];
+          return shuffleArray(newVisible);
+        });
+          setAllCards(allCards.slice(2));
+          setScoreMultiplier(prevState => prevState + 5)
+      }
     }
   }, [allCards, visibleCards])
+
+  function getHighScore() {
+    if (score > parseInt(highScore)) setHighScore(score.toString())
+  }
 
   function clickCard(clickedCard: Card) {
     if (clickedCard.selected) {
       console.log(`card: ${clickedCard.cardName} has already been selected! Game Over!`);
-      setLoserCard(clickedCard)
+      getHighScore()
+      setLoserCard([clickedCard])
       setGameOver(true)
+    } else {
+      setScore(prevState => prevState + scoreMultiplier)
+      setVisibleCards(prevCards => {
+        const updatedCards = prevCards.map(card =>
+          card.cardName === clickedCard.cardName
+            ? { ...card, selected: true }
+            : card
+        );
+        return shuffleArray(updatedCards);
+      });
     }
-
-    setVisibleCards(prevCards => {
-      const updatedCards = prevCards.map(card =>
-        card.cardName === clickedCard.cardName
-          ? { ...card, selected: true }
-          : card
-      );
-      return shuffleArray(updatedCards);
-    });
   }
 
   return (
@@ -75,7 +93,7 @@ export function App() {
           <h1>Game Over!</h1>
           <h3>You Selected this Card Twice!</h3>
           <Cards
-            cards={[loserCard!]}
+            cards={loserCard!}
           />
         </>
       )}
